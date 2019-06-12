b62c8a5113e2939d9abb034712ec43583e7d98bc
viki-korotkaya <viki.korotkaya@gmail.com>
2019-06-12 15:27:39 -0400

    Log out Link

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -9,7 +9,7 @@ const toolbar = (props) => (
 		<DrawerToggle clicked={props.drawerToggleClicked} />
 		<div className={classes.Logo}><Logo /></div>		
 		<nav className={classes.DesktopOnly}>
-			<NavigationItems />
+			<NavigationItems isAuthenticated={props.isAuth} />
 		</nav>
 
 	</header>
