668f3585bf683e18900bd62eebf9295ee72ff93a
viki-korotkaya <viki.korotkaya@gmail.com>
2019-06-12 10:49:57 -0400

    Adding Authenitication

---
+++ src/store/actions/auth.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCc07SrkoArQ8ERxHcgFjRSl9BfuByFriE';
+        if (!isSignup){
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCc07SrkoArQ8ERxHcgFjRSl9BfuByFriE';
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
+    };
+};
