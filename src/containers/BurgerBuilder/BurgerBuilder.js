8fb36b6bb43ed3aca2c43e6f41c7d955f6844768
viki-korotkaya <viki.korotkaya@gmail.com>
2019-06-28 22:05:02 -0400

    Added testing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import axios from "../../axios-orders";
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 
-class BurgerBuilder extends Component{
+export class BurgerBuilder extends Component{
     // constructor(props){
     //     super(props);
     //     this.state = {....}
@@ -62,8 +62,8 @@ class BurgerBuilder extends Component{
         };
         for (let key in disabledInfo){
             disabledInfo[key] = disabledInfo[key] <= 0
-        };
-        let orderSummary = null
+        }
+        let orderSummary = null;
         
         
         let burger = this.props.error ? <p>Ingredients can't be loaded"</p> : <Spinner />;
